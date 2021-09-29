c7ce2f2d8949139507a7ead642f72365da9f907b
SandraRusu17 <sandra.rusu17@gmail.com>
2021-09-29 14:49:27 +0300

    Implement CRUD methods for Dogs

--- src/main/java/com/p5/adoptions/service/AnimalShelterService.java
+++ src/main/java/com/p5/adoptions/service/AnimalShelterService.java
@@ -1,13 +1,16 @@
 package com.p5.adoptions.service;
 
 import com.p5.adoptions.repository.cats.Cat;
+import com.p5.adoptions.repository.dogs.Dog;
 import com.p5.adoptions.repository.shelters.AnimalShelter;
 import com.p5.adoptions.repository.shelters.AnimalShelterRepository;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AnimalShelterService {
@@ -36,11 +39,8 @@ public class AnimalShelterService {
     }
 
     public AnimalShelter findById(Integer id) {
-        Optional<AnimalShelter> optional = animalShelterRepository.findById(id);
-
-        return optional.orElseThrow(() -> new EntityNotFoundException("Shelter with id " + id + " not found"));
+        return getShelterById(id);
     }
-
 
     public void deleteShelter(Integer id) {
         animalShelterRepository.deleteById(id);
@@ -63,4 +63,71 @@ public class AnimalShelterService {
         catList.add(cat);
         return catList;
     }
+
+    public Cat updateCatInShelter(Integer shelterId, Integer catId, Cat cat) {
+        AnimalShelter shelter = getShelterById(shelterId);
+
+        List<Cat> newCats = shelter.getCats().stream().map(c -> {
+            if (c.getId().equals(catId)) {
+                cat.setId(catId);
+                return cat;
+            }
+            return c;
+        }).collect(Collectors.toList());
+        shelter.setCats(newCats);
+        animalShelterRepository.save(shelter);
+        return cat;
+    }
+
+    private AnimalShelter getShelterById(Integer id) {
+        Optional<AnimalShelter> optional = animalShelterRepository.findById(id);
+        return optional.orElseThrow(() -> new EntityNotFoundException("Shelter with id " + id + " not found"));
+    }
+
+    public void deleteCatInShelter(Integer shelterId, Integer catId) {
+        AnimalShelter shelter = getShelterById(shelterId);
+        List<Cat> newCats = shelter.getCats().stream().filter(c -> !c.getId().equals(catId)).collect(Collectors.toList());
+        shelter.setCats(newCats);
+        animalShelterRepository.save(shelter);
+    }
+
+    public List<Dog> findAllDogsByShelter(Integer shelterId) {
+        AnimalShelter shelter = getShelterById(shelterId);
+        return shelter.getDogs();
+    }
+
+    public List<Dog> addNewDogToShelter(Integer shelterId, Dog dog) {
+        AnimalShelter shelter = getShelterById(shelterId);
+        shelter.getDogs().add(dog);
+        animalShelterRepository.save(shelter);
+        return shelter.getDogs();
+    }
+
+    public Dog updateDogInShelter(Integer shelterId, Integer dogId, Dog dog) {
+        AnimalShelter shelter = getShelterById(shelterId);
+
+        List<Dog> newDogs = shelter.getDogs().stream().map(d -> {
+            if (d.getId().equals(dogId)) {
+                dog.setId(dogId);
+                return dog;
+            }
+            return d;
+        }).collect(Collectors.toList());
+        shelter.setDogs(newDogs);
+        animalShelterRepository.save(shelter);
+
+        return dog;
+    }
+
+    public void deleteDogInShelter(Integer shelterId, Integer dogId) {
+        AnimalShelter shelter = getShelterById(shelterId);
+        boolean removed = shelter.getDogs().removeIf(d -> d.getId().equals(dogId));
+        animalShelterRepository.save(shelter);
+
+        if (!removed) {
+            throw new RuntimeException("Already deleted or entity missing");
+        }
+    }
+
 }
+
